f34b6938a5e84ecf469183181b3ff437d1921454
Fktrctq <henrysaddler@gmail.com>
2018-04-30 17:39:52 +0400

    Added game modes and bugfixes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let numSquares = 6;
 let colors = randomColors(6);
 
 let squares = document.querySelectorAll(".square");
@@ -6,12 +7,15 @@ let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.getElementById("message");
 let h1 = document.querySelector("h1");
 let resetButton = document.getElementById("reset");
+let easyBtn = document.getElementById("easyBtn");
+let hardBtn = document.getElementById("hardBtn");
 
 colorDisplay.textContent = pickedColor;
 
+//RESET BUTTON
 resetButton.addEventListener("click", function() {
   //generate new colors
-  colors = randomColors(6);
+  colors = randomColors(numSquares);
   //pick new colors from array
   pickedColor = pickColor(colors);
   //change colorDisplay to match picked color
@@ -22,8 +26,45 @@ resetButton.addEventListener("click", function() {
     squares[i].style.backgroundColor = colors[i];
   }
   h1.style.backgroundColor = "#232323";
+  resetButton.textContent = "New Colors"; //bugfix
 });
 
+//EASY BUTTON
+easyBtn.addEventListener("click", function() {
+  numSquares = 3;
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  colors = randomColors(numSquares);
+  pickedColor = pickColor(colors);
+  colorDisplay.textContent = pickedColor;
+
+  //setting new colors and display:none to last divs
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+//HARD BUTTON
+hardBtn.addEventListener("click", function() {
+  numSquares = 6;
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  colors = randomColors(numSquares);
+  pickedColor = pickColor(colors);
+  colorDisplay.textContent = pickedColor;
+
+  //setting new colors and display:block to last divs
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+});
+
+//MAIN LOGIC
 for (let i = 0; i < squares.length; i++) {
   //add random colors to square divs
   squares[i].style.backgroundColor = colors[i];
@@ -44,6 +85,9 @@ for (let i = 0; i < squares.length; i++) {
     }
   });
 }
+
+//____________________________________________________________________
+//FUNCTIONS
 
 //change colors to correct one if winning condition met
 function changeColors(color) {
